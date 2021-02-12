874b990e1468859d1fd4594038cfefd68169f5ab
OPAS KEATTIPECHCHARAT <opas.miracle@gmail.com>
2021-02-12 11:26:04 +0700

    reset dir

--- models/responseHTTP.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// ResponseHTTP represents response body of this API
-type ResponseHTTP struct {
-	Success bool        `json:"success"`
-	Data    interface{} `json:"data"`
-	Message string      `json:"message"`
-}
